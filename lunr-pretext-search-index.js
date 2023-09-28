2bf079d8e03dfdbd2a75cd824ef6bbf0f85e81ec
suohyd <86884865+suohyd@users.noreply.github.com>
2023-09-28 11:29:31 +0000

    Latest build deployed.

--- lunr-pretext-search-index.js
+++ lunr-pretext-search-index.js
@@ -24,8 +24,8 @@ var ptx_lunr_docs = [
   "url": "sec-basic-operations.html",
   "type": "Section",
   "number": "1.1",
-  "title": "Questions related to basic matrix operations",
-  "body": " Questions related to basic matrix operations   (Zero matrix)   We denote by the matrix with all its entries zero. We call the zero matrix.     (Identity matrix)   We denote by the matrix with all its diagonal entries , and all other (non-diagonal) entries zero. We call the identity matrix.     We denote the set of all matrices over by . So . For any matrix and we denote by the matrix obtained by multiplying every entry of the matrix by .    Suppose that and . We denote the -th entry, i.e., entry in the -th row and -th column of the matrix (respectively, ) by (respectively, ). The -th entry of the matrix multiplication is given by the following formula. where and .     Consider the following matrices in . Find .    Let . Find such that     Show that for any matrix the following is true.     Show that for any matrices the following is true.     Let . Show that and that .    Compute the matrix multiplication .    and       and             and  In this case, consider the matrix as the point of the space and matrix as the point . Plot the line segment joining the origin of and as well as the line segment joining the origin of and . Check whether is perpendicular to .       and   In this case, consider the matrix as the point of the space and matrix as the point . Plot the line segment joining the origin of and as well as the line segment joining the origin of and . Check whether is perpendicular to .        For a matrix and a natural number we write In particular, and .  Find the following powers of the given matrices.      Write the augmented matrix corresponding to the given system of equations.                 (Transpose of a matrix)  For a matrix , the transpose of , denoted by , is an matrix obtained by writing columns of as rows of . Thus if then the transpose of is    In the following, given compute its transpose and also compute and .     , where      , where                    Find a condition on such that the matrix will be equal to its transpose.    Consider a matrix . Find a condition on such that and .      Check whether is a function or not. Also check whether is injective (one-one) or surjective (onto).     "
+  "title": "Basic matrix operations",
+  "body": " Basic matrix operations   (Zero matrix)   We denote by the matrix with all its entries zero. We call the zero matrix.     (Identity matrix)   We denote by the matrix with all its diagonal entries , and all other (non-diagonal) entries zero. We call the identity matrix.     We denote the set of all matrices over by . So . For any matrix and we denote by the matrix obtained by multiplying every entry of the matrix by .    Suppose that and . We denote the -th entry, i.e., entry in the -th row and -th column of the matrix (respectively, ) by (respectively, ). The -th entry of the matrix multiplication is given by the following formula. where and .    (Transpose of a matrix)  For a matrix , the transpose of , denoted by , is an matrix obtained by writing columns of as rows of . Thus if then the transpose of is    "
 },
 {
   "id": "def-zero-matrix",
@@ -79,7 +79,7 @@ var ptx_lunr_docs = [
   "type": "Exercises",
   "number": "1.2",
   "title": "Exercises",
-  "body": "  A few interactive practice exercises are listed below.    Multiplication is defined when and .    For any two matrices , .   For any , .       The following system of linear equations has infinitely many solutions.     For the following matrix we have .    "
+  "body": "  A few exercises related to basic matrix operations are listed below.   Consider the following matrices in . Find .   Let . Find such that .    Show that for any matrix the following is true.     Show that for any matrices the following is true.     Let . Show that and that .    Compute the matrix multiplication .    and       and             and  In this case, consider the matrix as the point of the space and matrix as the point . Plot the line segment joining the origin of and as well as the line segment joining the origin of and . Check whether is perpendicular to .       and   In this case, consider the matrix as the point of the space and matrix as the point . Plot the line segment joining the origin of and as well as the line segment joining the origin of and . Check whether is perpendicular to .        For a matrix and a natural number we write In particular, and .  Find the following powers of the given matrices.     Write the augmented matrix corresponding to the given system of equations.                In the following, given compute its transpose and also compute and .     , where      , where                    Find a condition on such that the matrix will be equal to its transpose.    Consider a matrix . Find a condition on such that and .      Check whether is a function or not. Also check whether is injective (one-one) or surjective (onto).     Multiplication is defined when and .    For any two matrices , .   For any , .       The following system of linear equations has infinitely many solutions.     For the following matrix we have .    "
 },
 {
   "id": "exercise-1",
@@ -88,7 +88,7 @@ var ptx_lunr_docs = [
   "type": "Exercise",
   "number": "1.2.1",
   "title": "",
-  "body": " Multiplication is defined when and .  "
+  "body": "Consider the following matrices in . Find . "
 },
 {
   "id": "exercise-2",
@@ -97,7 +97,7 @@ var ptx_lunr_docs = [
   "type": "Exercise",
   "number": "1.2.2",
   "title": "",
-  "body": " For any two matrices , .  "
+  "body": " Let . Find such that .  "
 },
 {
   "id": "exercise-3",
@@ -106,7 +106,7 @@ var ptx_lunr_docs = [
   "type": "Exercise",
   "number": "1.2.3",
   "title": "",
-  "body": "For any , . "
+  "body": " Show that for any matrix the following is true.   "
 },
 {
   "id": "exercise-4",
@@ -115,7 +115,7 @@ var ptx_lunr_docs = [
   "type": "Exercise",
   "number": "1.2.4",
   "title": "",
-  "body": "   "
+  "body": " Show that for any matrices the following is true.   "
 },
 {
   "id": "exercise-5",
@@ -124,7 +124,7 @@ var ptx_lunr_docs = [
   "type": "Exercise",
   "number": "1.2.5",
   "title": "",
-  "body": " The following system of linear equations has infinitely many solutions.   "
+  "body": " Let . Show that and that .  "
 },
 {
   "id": "exercise-6",
@@ -132,6 +132,96 @@ var ptx_lunr_docs = [
   "url": "practice-basic-operations.html#exercise-6",
   "type": "Exercise",
   "number": "1.2.6",
+  "title": "",
+  "body": " Compute the matrix multiplication .    and       and             and  In this case, consider the matrix as the point of the space and matrix as the point . Plot the line segment joining the origin of and as well as the line segment joining the origin of and . Check whether is perpendicular to .       and   In this case, consider the matrix as the point of the space and matrix as the point . Plot the line segment joining the origin of and as well as the line segment joining the origin of and . Check whether is perpendicular to .      "
+},
+{
+  "id": "exercise-7",
+  "level": "2",
+  "url": "practice-basic-operations.html#exercise-7",
+  "type": "Exercise",
+  "number": "1.2.7",
+  "title": "",
+  "body": " For a matrix and a natural number we write In particular, and .  Find the following powers of the given matrices.   "
+},
+{
+  "id": "exercise-8",
+  "level": "2",
+  "url": "practice-basic-operations.html#exercise-8",
+  "type": "Exercise",
+  "number": "1.2.8",
+  "title": "",
+  "body": " Write the augmented matrix corresponding to the given system of equations.              "
+},
+{
+  "id": "exercise-9",
+  "level": "2",
+  "url": "practice-basic-operations.html#exercise-9",
+  "type": "Exercise",
+  "number": "1.2.9",
+  "title": "",
+  "body": " In the following, given compute its transpose and also compute and .     , where      , where                    Find a condition on such that the matrix will be equal to its transpose.    Consider a matrix . Find a condition on such that and .    "
+},
+{
+  "id": "exercise-10",
+  "level": "2",
+  "url": "practice-basic-operations.html#exercise-10",
+  "type": "Exercise",
+  "number": "1.2.10",
+  "title": "",
+  "body": " Check whether is a function or not. Also check whether is injective (one-one) or surjective (onto).   "
+},
+{
+  "id": "exercise-11",
+  "level": "2",
+  "url": "practice-basic-operations.html#exercise-11",
+  "type": "Exercise",
+  "number": "1.2.11",
+  "title": "",
+  "body": " Multiplication is defined when and .  "
+},
+{
+  "id": "exercise-12",
+  "level": "2",
+  "url": "practice-basic-operations.html#exercise-12",
+  "type": "Exercise",
+  "number": "1.2.12",
+  "title": "",
+  "body": " For any two matrices , .  "
+},
+{
+  "id": "exercise-13",
+  "level": "2",
+  "url": "practice-basic-operations.html#exercise-13",
+  "type": "Exercise",
+  "number": "1.2.13",
+  "title": "",
+  "body": "For any , . "
+},
+{
+  "id": "exercise-14",
+  "level": "2",
+  "url": "practice-basic-operations.html#exercise-14",
+  "type": "Exercise",
+  "number": "1.2.14",
+  "title": "",
+  "body": "   "
+},
+{
+  "id": "exercise-15",
+  "level": "2",
+  "url": "practice-basic-operations.html#exercise-15",
+  "type": "Exercise",
+  "number": "1.2.15",
+  "title": "",
+  "body": " The following system of linear equations has infinitely many solutions.   "
+},
+{
+  "id": "exercise-16",
+  "level": "2",
+  "url": "practice-basic-operations.html#exercise-16",
+  "type": "Exercise",
+  "number": "1.2.16",
   "title": "",
   "body": " For the following matrix we have .   "
 },
@@ -141,8 +231,8 @@ var ptx_lunr_docs = [
   "url": "sec-row-echelon-form.html",
   "type": "Section",
   "number": "1.3",
-  "title": "Questions related to row reduced echelon form of a matrix",
-  "body": " Questions related to row reduced echelon form of a matrix   (Elementary row operations)   Let . Denote by (for ) the -th row of . Following are elementary row operations.   Interchanging -th row with -th row. This operation is usually denoted by .    Multiplying -th row by a nonzero . This operation is usually denoted by .    Adding a constant multiple of -th row to -th row. This operation is usually denoted by .        Procedure to obtain a row reduced echelon form of a matrix Recall that given a matrix , by applying a sequence of elementary row operations, can be reduced to a row-reduced echelon form of . The matrix has the following properties.  All zero rows occurs at the bottom of .  If a row of is nonzero then the first nonzero entry is , it is called pivot .  In any two successive nonzero rows of , the pivot in the lower row occurs to the right of the pivot in the higher row.  Each column of that contains the pivot has zero everywhere else.         Find the row-reduced echelon form of the following matrices. .    How many pivots are there in one row? How many pivot positions are there in one column?    In a matrix what is the largest possible number of pivots?    Write the row-reduced echelon matrix whose every row has a pivot.    Consider the following augmented matrices. In each case conssider the corresponding system of linear equations and determine whether the system has no solution, a unique solution, or infinitely many solutions.                      "
+  "title": "Row reduced echelon form of a matrix",
+  "body": " Row reduced echelon form of a matrix   (Elementary row operations)   Let . Denote by (for ) the -th row of . Following are elementary row operations.   Interchanging -th row with -th row. This operation is usually denoted by .    Multiplying -th row by a nonzero . This operation is usually denoted by .    Adding a constant multiple of -th row to -th row. This operation is usually denoted by .        Procedure to obtain a row reduced echelon form of a matrix Recall that given a matrix , by applying a sequence of elementary row operations, can be reduced to a row-reduced echelon form of . The matrix has the following properties.  All zero rows occurs at the bottom of .  If a row of is nonzero then the first nonzero entry is , it is called pivot .  In any two successive nonzero rows of , the pivot in the lower row occurs to the right of the pivot in the higher row.  Each column of that contains the pivot has zero everywhere else.    "
 },
 {
   "id": "def-elementary-row-operations",
@@ -161,6 +251,60 @@ var ptx_lunr_docs = [
   "number": "1.3.2",
   "title": "Procedure to obtain a row reduced echelon form of a matrix.",
   "body": " Procedure to obtain a row reduced echelon form of a matrix Recall that given a matrix , by applying a sequence of elementary row operations, can be reduced to a row-reduced echelon form of . The matrix has the following properties.  All zero rows occurs at the bottom of .  If a row of is nonzero then the first nonzero entry is , it is called pivot .  In any two successive nonzero rows of , the pivot in the lower row occurs to the right of the pivot in the higher row.  Each column of that contains the pivot has zero everywhere else.   "
+},
+{
+  "id": "practice-row-echelon-form",
+  "level": "1",
+  "url": "practice-row-echelon-form.html",
+  "type": "Exercises",
+  "number": "1.4",
+  "title": "Exercises",
+  "body": "  A few exercises are related to row reduced echelon form of a matrix are listed below.   Find the row-reduced echelon form of the following matrices. .  How many pivots are there in one row? How many pivot positions are there in one column?  In a matrix what is the largest possible number of pivots?  Write the row-reduced echelon matrix whose every row has a pivot.  Consider the following augmented matrices. In each case conssider the corresponding system of linear equations and determine whether the system has no solution, a unique solution, or infinitely many solutions.                   "
+},
+{
+  "id": "exercise-17",
+  "level": "2",
+  "url": "practice-row-echelon-form.html#exercise-17",
+  "type": "Exercise",
+  "number": "1.4.1",
+  "title": "",
+  "body": "Find the row-reduced echelon form of the following matrices. . "
+},
+{
+  "id": "exercise-18",
+  "level": "2",
+  "url": "practice-row-echelon-form.html#exercise-18",
+  "type": "Exercise",
+  "number": "1.4.2",
+  "title": "",
+  "body": "How many pivots are there in one row? How many pivot positions are there in one column? "
+},
+{
+  "id": "exercise-19",
+  "level": "2",
+  "url": "practice-row-echelon-form.html#exercise-19",
+  "type": "Exercise",
+  "number": "1.4.3",
+  "title": "",
+  "body": "In a matrix what is the largest possible number of pivots? "
+},
+{
+  "id": "exercise-20",
+  "level": "2",
+  "url": "practice-row-echelon-form.html#exercise-20",
+  "type": "Exercise",
+  "number": "1.4.4",
+  "title": "",
+  "body": "Write the row-reduced echelon matrix whose every row has a pivot. "
+},
+{
+  "id": "exercise-21",
+  "level": "2",
+  "url": "practice-row-echelon-form.html#exercise-21",
+  "type": "Exercise",
+  "number": "1.4.5",
+  "title": "",
+  "body": "Consider the following augmented matrices. In each case conssider the corresponding system of linear equations and determine whether the system has no solution, a unique solution, or infinitely many solutions.                  "
 },
 {
   "id": "colophon-2",
